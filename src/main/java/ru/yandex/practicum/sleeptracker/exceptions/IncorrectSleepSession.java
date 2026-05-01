b6632ca667ddf9e9b3ee821e2b7d5b55c3da108a
Stepan Kholodov <kholodov_stepan@inbox.ru>
2026-05-01 19:45:34 +0300

    Исправлена ошибка codeStyle

--- src/main/java/ru/yandex/practicum/sleeptracker/exceptions/IncorrectSleepSession.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/exceptions/IncorrectSleepSession.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.sleeptracker.exceptions;
 
-public class IncorrectSleepSession extends RuntimeException{
+public class IncorrectSleepSession extends RuntimeException {
     public IncorrectSleepSession(String message) {
         super(message);
     }
